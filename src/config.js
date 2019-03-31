1826cac454b01e54bbf62e3918faad9ab5fb49fe
MOTORIST <webartmotor@gmail.com>
2019-03-31 11:27:38 +0300

    refactor: production config

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const prod = {
   API_URL: 'http://84.201.150.106:8080/api',
 };
 
-const config = process.env.REACT_APP_STAGE === 'production' ? prod : dev;
+const config = process.env.NODE_ENV === 'production' ? prod : dev;
 
 export default {
   ...config,
